9046bf0e40d6cd2abb6847b10384e79d81c83362
zh0glikk <51762977+zh0glikk@users.noreply.github.com>
2020-11-05 14:46:11 +0200

    [develop] STL

---
+++ STL/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include "TextHandler.h"
+
+int main() {
+    TextHandler* handler = new TextHandler("symbols.txt");
+
+    handler->parseText();
+
+    std::cout << *handler << std::endl;
+
+
+    return 0;
+}
